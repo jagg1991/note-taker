dc88ff2c041410e4e2f855a477288de88f242ab0
jagg1991 <josephguajardo@ymail.com>
2021-03-11 17:01:40 -0800

    adding to server

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-//importing express 
+//importing dependencies 
 const express = require('express');
-const path = require('path');
-//creating port 
-const app = express();
-const PORT = 3000;
 
+//creating port 
+//tells node that we are creating an express server
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// sets up the Express app to handle data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// sets public as the root folder when coming from front end. 
+app.use(express.static('public'));
+
+//api routes
+require('.routes/htmlRoutes')(app);
+
+require('/routes/api-routes')(app)
+
+
+//LISTENER 
+//the below code starts the server
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
